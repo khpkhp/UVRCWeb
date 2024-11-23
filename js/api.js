7a541263115857c139f7b6fb43d294ecea9b1b55
Khanh Thanh <100078354+khanhthanhdev@users.noreply.github.com>
2024-11-23 17:08:01 +0000

    add crud

--- js/api.js
+++ js/api.js
@@ -1,71 +1,139 @@
 // API functions for different match types
 const api = {
+    // Generic CRUD operations
+    async create(endpoint, data) {
+        return await config.fetchApi(endpoint, {
+            method: 'POST',
+            body: JSON.stringify(data)
+        });
+    },
+
+    async read(endpoint, id = null) {
+        const url = id ? `${endpoint}/${id}` : endpoint;
+        return await config.fetchApi(url);
+    },
+
+    async update(endpoint, id, data) {
+        return await config.fetchApi(`${endpoint}/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(data)
+        });
+    },
+
+    async delete(endpoint, id) {
+        return await config.fetchApi(`${endpoint}/${id}`, {
+            method: 'DELETE'
+        });
+    },
+
     // QM Matches
     async loadQMMatches() {
-        return await config.fetchApi('qmmatches');
+        return await this.read('qmmatches');
     },
     async loadQMMatch(matchnum) {
-        return await config.fetchApi(`qmmatches/${matchnum}`);
+        return await this.read('qmmatches', matchnum);
     },
     async saveQMMatch(matchData) {
-        return await config.fetchApi(`qmmatches/${matchData.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(matchData)
-        });
+        if (matchData.id) {
+            return await this.update('qmmatches', matchData.id, matchData);
+        } else {
+            return await this.create('qmmatches', matchData);
+        }
+    },
+    async deleteQMMatch(matchnum) {
+        return await this.delete('qmmatches', matchnum);
     },
 
     // PM Matches
     async loadPMMatches() {
-        return await config.fetchApi('pmmatches');
+        return await this.read('pmmatches');
     },
     async loadPMMatch(matchnum) {
-        return await config.fetchApi(`pmmatches/${matchnum}`);
+        return await this.read('pmmatches', matchnum);
     },
     async savePMMatch(matchData) {
-        return await config.fetchApi(`pmmatches/${matchData.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(matchData)
-        });
+        if (matchData.id) {
+            return await this.update('pmmatches', matchData.id, matchData);
+        } else {
+            return await this.create('pmmatches', matchData);
+        }
+    },
+    async deletePMMatch(matchnum) {
+        return await this.delete('pmmatches', matchnum);
     },
 
     // PO Matches
     async loadPOMatches() {
-        return await config.fetchApi('pomatches');
+        return await this.read('pomatches');
     },
     async loadPOMatch(matchnum) {
-        return await config.fetchApi(`pomatches/${matchnum}`);
+        return await this.read('pomatches', matchnum);
     },
     async savePOMatch(matchData) {
-        return await config.fetchApi(`pomatches/${matchData.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(matchData)
-        });
+        if (matchData.id) {
+            return await this.update('pomatches', matchData.id, matchData);
+        } else {
+            return await this.create('pomatches', matchData);
+        }
+    },
+    async deletePOMatch(matchnum) {
+        return await this.delete('pomatches', matchnum);
     },
 
     // Teams
     async loadTeams() {
-        return await config.fetchApi('team');
+        return await this.read('team');
+    },
+    async loadTeam(teamId) {
+        return await this.read('team', teamId);
     },
     async saveTeam(teamData) {
-        return await config.fetchApi(`team/${teamData.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(teamData)
-        });
+        if (teamData.id) {
+            return await this.update('team', teamData.id, teamData);
+        } else {
+            return await this.create('team', teamData);
+        }
+    },
+    async deleteTeam(teamId) {
+        return await this.delete('team', teamId);
     },
 
     // Results
     async loadResults() {
-        return await config.fetchApi('results');
+        return await this.read('results');
+    },
+    async loadResult(resultId) {
+        return await this.read('results', resultId);
+    },
+    async saveResult(resultData) {
+        if (resultData.id) {
+            return await this.update('results', resultData.id, resultData);
+        } else {
+            return await this.create('results', resultData);
+        }
+    },
+    async deleteResult(resultId) {
+        return await this.delete('results', resultId);
     },
 
     // PO Teams
     async loadPOTeams() {
-        return await config.fetchApi('poteams');
+        return await this.read('poteams');
     },
     async savePOTeams(teamData) {
-        return await config.fetchApi(`poteams/${teamData.id}`, {
-            method: 'PUT',
-            body: JSON.stringify(teamData)
-        });
+        if (teamData.id) {
+            return await this.update('poteams', teamData.id, teamData);
+        } else {
+            return await this.create('poteams', teamData);
+        }
+    },
+    async deletePOTeam(teamId) {
+        return await this.delete('poteams', teamId);
+    },
+
+    // Error handling helper
+    handleError(error) {
+        console.error('API Error:', error);
+        config.showError(error.message || 'An error occurred while processing your request');
     }
 };
